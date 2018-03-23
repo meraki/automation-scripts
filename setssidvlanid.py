1248c82593b3a231fa7297ba94d32aa4f25c3e3b
mpapazog <spamandviruses@gmail.com>
2018-03-23 10:55:36 +0200

    Add support for net tag filter

--- setssidvlanid.py
+++ setssidvlanid.py
@@ -1,5 +1,13 @@
 # This is a script to set the VLAN on an SSID. Usage:
-#  python setssidvlanid.py -k <api_key> -o <org_name> -n <ssid_name> -v <vlan_id>
+#  python setssidvlanid.py -k <api_key> -o <org_name> -n <ssid_name> -v <vlan_id> [-t <net_tag>]
+#
+# Mandatory arguments: 
+#   -k <api_key>        Your Meraki Dashboard API key
+#   -o <org_name>       Name of the organization you want to be processed
+#   -n <ssid_name>      Name of the SSID to be processed
+#   -v <vlan_id>        New VLAN ID (number) for the SSID
+# Optional argument:
+#   -t <net_tag>        Only process networks tagged <net_tag>
 #
 # Example:
 #  python setssidvlanid.py -k 1234 -o "Meraki Inc" -n "Meraki corp" -v 10
@@ -9,7 +17,7 @@
 #
 # To pass parameters containing spaces in Windows, use double quotes "".
 #
-# This file was last modified on 2018-03-20
+# This file was last modified on 2018-03-23
 
 import sys, getopt, requests, json, time
 from datetime import datetime
@@ -24,12 +32,12 @@ REQUESTS_READ_TIMEOUT    = 30
 #used by merakirequestthrottler(). DO NOT MODIFY
 LAST_MERAKI_REQUEST = datetime.now()
 
-def merakirequestthrottler(p_requestcount=1):
+def merakirequestthrottler():
     #makes sure there is enough time between API requests to Dashboard not to hit shaper
     global LAST_MERAKI_REQUEST
     
-    if (datetime.now()-LAST_MERAKI_REQUEST).total_seconds() < (API_EXEC_DELAY*p_requestcount):
-        time.sleep(API_EXEC_DELAY*p_requestcount)
+    if (datetime.now()-LAST_MERAKI_REQUEST).total_seconds() < API_EXEC_DELAY:
+        time.sleep(API_EXEC_DELAY)
     
     LAST_MERAKI_REQUEST = datetime.now()
     return 
@@ -45,7 +53,7 @@ def printhelp():
     #prints help text
 
     printusertext('This is a script to set the VLAN on an SSID. Usage:')
-    printusertext('    python setssidvlanid.py -k <api_key> -o <org_name> -n <ssid_name> -v <vlan_id>')
+    printusertext('    python setssidvlanid.py -k <api_key> -o <org_name> -n <ssid_name> -v <vlan_id> [-t <net_tag>]')
     printusertext('')
     printusertext('Example:')
     printusertext('    python setssidvlanid.py -k 1234 -o "Meraki Inc" -n "Meraki corp" -v 10')
@@ -132,17 +140,18 @@ def setssidattribute(p_apikey, p_shardhost, p_netid, p_ssidnum, p_attribute, p_v
 def main(argv):
     printusertext('INFO: Script started at %s' % datetime.now())
     
-    #   python setssidvlanid.py -k <api_key> -o <org_name> -n <ssid_name> -v <vlan_id>
+    #   python setssidvlanid.py -k <api_key> -o <org_name> -n <ssid_name> -v <vlan_id> [-t <net_tag>]
 
     #initialize variables for command line arguments
     arg_apikey      = ''
     arg_orgname     = ''
     arg_ssidname    = ''
     arg_vlanid      = ''
+    arg_nettag      = ''
     
     #get command line arguments
     try:
-        opts, args = getopt.getopt(argv, 'hk:o:n:v:')
+        opts, args = getopt.getopt(argv, 'hk:o:n:v:t:')
     except getopt.GetoptError:
         printhelp()
         sys.exit(2)
@@ -159,11 +168,16 @@ def main(argv):
             arg_ssidname = arg
         elif opt == '-v':
             arg_vlanid  = arg
+        elif opt == '-t':
+            arg_nettag  = arg
             
     #check if all required parameters have been given
     if arg_apikey == '' or arg_orgname == '' or arg_ssidname == '' or arg_vlanid == '':
         printhelp()
         sys.exit(2)
+        
+    #set operational mode flags (True/False)
+    flag_modematchtag = (arg_nettag != '')    
         
     #resolve orgid    
     orgid = getorgid(arg_apikey, arg_orgname) 
@@ -179,24 +193,42 @@ def main(argv):
         sys.exit(2)
         
     for net in netlist:
+        #check that network has required tag, if user has given one
+        flag_tagmatchsuccess = True
+        if flag_modematchtag:
+            if net['tags'] is None:
+                flag_tagmatchsuccess = False
+            else:
+                if net['tags'].find(arg_nettag) == -1:
+                    flag_tagmatchsuccess = False
     
-        #get SSID list
-        ssidlist = getssids(arg_apikey, 'api.meraki.com', net['id'])
-        
-        if ssidlist[0]['number'] == 'null':
-            printusertext('WARNING: Skipping network "%s": contains no MRs' % net['name'])
+        if flag_tagmatchsuccess:
+            #get SSID list
+            ssidlist = getssids(arg_apikey, 'api.meraki.com', net['id'])
+            
+            if ssidlist[0]['number'] == 'null':
+                printusertext('WARNING: Skipping network "%s": No MR config' % net['name'])
+            else:
+                flag_ssidnotfound = True
+                #get SSID number corresponding to SSID name
+                for ssid in ssidlist:
+                    if ssid['name'] == arg_ssidname:
+                        printusertext('INFO: Setting VLAN ID "%s" for net "%s", SSID %s "%s"' % (arg_vlanid, net['name'], ssid['number'], ssid['name']))
+                        
+                        #set VLAN for correct SSID number
+                        if ssid['ipAssignmentMode'] in ['Layer 3 roaming with a concentrator', 'VPN']:
+                            flag_ssidnotfound = False
+                            setssidattribute(arg_apikey, 'api.meraki.com', net['id'], ssid['number'], 'vlanId', arg_vlanid)
+
+                        break #SSID name is unique, so no need to check the rest
+                
+                if flag_ssidnotfound:
+                    printusertext('WARNING: Skipping network "%s": no SSID "%s" or wrong addressing mode' % (net['name'], arg_ssidname))
+                        
         else:
-            #get SSID number corresponding to SSID name
-            for ssid in ssidlist:
-                if ssid['name'] == arg_ssidname:
-                    printusertext('INFO: Setting VLAN ID "%s" for net "%s", SSID %s "%s"' % (arg_vlanid, net['name'], ssid['number'], ssid['name']))
-                    
-                    #set VLAN for correct SSID number
-                    if ssid['ipAssignmentMode'] in ['Layer 3 roaming with a concentrator', 'VPN']:
-                        setssidattribute(arg_apikey, 'api.meraki.com', net['id'], ssid['number'], 'vlanId', arg_vlanid)
-                    
-                    break #SSID name is unique, so no need to check the rest
+            printusertext('WARNING: Skipping network "%s": No tag "%s"' % (net['name'], arg_nettag))
     
+    printusertext('INFO: Reached end of script at %s' % datetime.now())
     
 if __name__ == '__main__':
     main(sys.argv[1:])
