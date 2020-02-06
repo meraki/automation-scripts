9f83c419044d1fdc7437a6a6d170f066a141b0c8
mpapazog <spamandviruses@gmail.com>
2020-02-06 15:53:43 +0200

    "-f /all" no longer exits if org has API disabled
    Now script displays a warning instead of exiting completely when unable to pull inventory of an org, allowing admins who have access to orgs with API disabled to run with parameter "-f /all".

--- merakidevicecounts.py
+++ merakidevicecounts.py
@@ -1,25 +1,22 @@
-# This is a script to count device types in multiple orgs.
-#
-# Usage:
-#  python merakidevicecounts.py -k <key> -f <file>
-#
-# Arguments:
-#  -k <key>  : Your Meraki Dashboard API key
-#  -f <file> : File with list of organizations to be counted. Use "-f /all" to count all organizations
-#
-# Examples:
-#  python merakidevicecounts.py -k 1234 -f /all
-#  python merakidevicecounts.py -k 1234 -f orglist.txt
-#
-# Creating an input file:
-#  Use a text editor to create a text file, where organization names are listed one per line
-#
-# To make script chaining easier, all lines containing informational messages to the user
-#  start with the character @
-#
-# This file was last modified on 2017-10-24
+readMe = '''
+This is a script to count device types in multiple orgs.
 
-import sys, getopt, requests, json, time
+Usage:
+  python merakidevicecounts.py -k <key> -f <file>
+
+Arguments:
+  -k <key>  : Your Meraki Dashboard API key
+  -f <file> : File with list of organizations to be counted. Use "-f /all" to count all organizations
+
+Examples:
+  python merakidevicecounts.py -k 1234 -f /all
+  python merakidevicecounts.py -k 1234 -f orglist.txt
+
+Creating an input file:
+  Use a text editor to create a text file, where organization names are listed one per line
+'''
+
+import sys, getopt, requests, json, time, datetime
 
 class c_devicedata:
     def __init__(self):
@@ -33,46 +30,48 @@ class c_organizationdata:
         self.id        = ''
         self.shardhost = ''
         self.devices   = []
+        self.skipMe    = False
 #end class   
+
+API_EXEC_DELAY              = 0.21 #Used in merakiRequestThrottler() to avoid hitting dashboard API max request rate
+
+#connect and read timeouts for the Requests module in seconds
+REQUESTS_CONNECT_TIMEOUT    = 60
+REQUESTS_READ_TIMEOUT       = 60
+
+LAST_MERAKI_REQUEST         = datetime.datetime.now()   #used by merakiRequestThrottler()
 
 #Used for time.sleep(API_EXEC_DELAY). Delay added to avoid hitting dashboard API max request rate
 API_EXEC_DELAY = 0.21
 
-def printusertext(p_message):
-    #prints a line of text that is meant for the user to read
-    #do not process these lines when chaining scripts
-    print('@ %s' % p_message)
-
 def printhelp():
     #prints help text
 
-    printusertext('This is a script to count device types in multiple orgs.')
-    printusertext('')
-    printusertext('Usage:')
-    printusertext(' python merakidevicecounts.py -k <key> -f <file>')
-    printusertext('')
-    printusertext('Arguments:')
-    printusertext(' -k <key>  : Your Meraki Dashboard API key')
-    printusertext(' -f <file> : File with list of organizations to be counted. Use "-f /all" to count all organizations')
-    printusertext('')
-    printusertext('Examples:')
-    printusertext(' python merakidevicecounts.py -k 1234 -f /all')
-    printusertext(' python merakidevicecounts.py -k 1234 -f orglist.txt')
-    printusertext('')
-    printusertext('Creating an input file:')
-    printusertext(' Use a text editor to create a text file, where organization names are listed one per line')
-    printusertext('')
-    printusertext('Use double quotes ("") in Windows to pass arguments containing spaces. Names are case-sensitive.')
+    print(readMe)
+    sys.exit(0)
+    
+def merakiRequestThrottler():
+    #prevents hitting max request rate shaper of the Meraki Dashboard API
+    global LAST_MERAKI_REQUEST
+    
+    if (datetime.datetime.now()-LAST_MERAKI_REQUEST).total_seconds() < (API_EXEC_DELAY):
+        time.sleep(API_EXEC_DELAY)
+    
+    LAST_MERAKI_REQUEST = datetime.datetime.now()
+    return
     
 def getorglist(p_apikey):
     #returns the organizations' list for a specified admin
     #DEBUG unfinished untested
     
-    time.sleep(API_EXEC_DELAY)
+    merakiRequestThrottler()
     try:
-        r = requests.get('https://dashboard.meraki.com/api/v0/organizations', headers={'X-Cisco-Meraki-API-Key': p_apikey, 'Content-Type': 'application/json'})
+        r = requests.get('https://api.meraki.com/api/v0/organizations', 
+            headers={'X-Cisco-Meraki-API-Key': p_apikey, 
+            'Content-Type': 'application/json'},
+            timeout=(REQUESTS_CONNECT_TIMEOUT, REQUESTS_READ_TIMEOUT))
     except:
-        printusertext('ERROR 00: Unable to contact Meraki cloud')
+        print('ERROR 00: Unable to contact Meraki cloud')
         sys.exit(2)
     
     returnvalue = []
@@ -88,11 +87,14 @@ def getorgid(p_apikey, p_orgname):
     #looks up org id for a specific org name
     #on failure returns 'null'
     
-    time.sleep(API_EXEC_DELAY)
+    merakiRequestThrottler()
     try:
-        r = requests.get('https://dashboard.meraki.com/api/v0/organizations', headers={'X-Cisco-Meraki-API-Key': p_apikey, 'Content-Type': 'application/json'})
+        r = requests.get('https://api.meraki.com/api/v0/organizations', 
+            headers={'X-Cisco-Meraki-API-Key': p_apikey, 
+            'Content-Type': 'application/json'},
+            timeout=(REQUESTS_CONNECT_TIMEOUT, REQUESTS_READ_TIMEOUT))
     except:
-        printusertext('ERROR 01: Unable to contact Meraki cloud')
+        print('ERROR 01: Unable to contact Meraki cloud')
         sys.exit(2)
     
     if r.status_code != requests.codes.ok:
@@ -104,61 +106,24 @@ def getorgid(p_apikey, p_orgname):
         if record['name'] == p_orgname:
             return record['id']
     return('null')
-    
-def getshardhost(p_apikey, p_orgid):
-    #Looks up shard URL for a specific org. Use this URL instead of 'dashboard.meraki.com'
-    # when making API calls with API accounts that can access multiple orgs.
-    #On failure returns 'null'
         
-    time.sleep(API_EXEC_DELAY)
-    try:
-        r = requests.get('https://dashboard.meraki.com/api/v0/organizations/%s/snmp' % p_orgid, headers={'X-Cisco-Meraki-API-Key': p_apikey, 'Content-Type': 'application/json'})
-    except:
-        printusertext('ERROR 02: Unable to contact Meraki cloud')
-        sys.exit(2)
     
-    if r.status_code != requests.codes.ok:
-        return 'null'
-        
-    rjson = r.json()
-    
-    return(rjson['hostname'])
-    
-def getnwid(p_apikey, p_shardhost, p_orgid, p_nwname):
-    #looks up network id for a network name
-    #on failure returns 'null'
-
-    time.sleep(API_EXEC_DELAY)
-    try:
-        r = requests.get('https://%s/api/v0/organizations/%s/networks' % (p_shardhost, p_orgid), headers={'X-Cisco-Meraki-API-Key': p_apikey, 'Content-Type': 'application/json'})
-    except:
-        printusertext('ERROR 03: Unable to contact Meraki cloud')
-        sys.exit(2)
-        
-    if r.status_code != requests.codes.ok:
-        return 'null'
-    
-    rjson = r.json()
-    
-    for record in rjson:
-        if record['name'] == p_nwname:
-            return record['id']
-    return('null') 
-    
-    
-def getorginventory(p_apikey, p_shardhost, p_orgid):
+def getorginventory(p_apikey, p_orgid):
     #returns full org inventory
 
-    time.sleep(API_EXEC_DELAY)
+    merakiRequestThrottler()
     try:
-        r = requests.get('https://%s/api/v0/organizations/%s/inventory' % (p_shardhost, p_orgid), headers={'X-Cisco-Meraki-API-Key': p_apikey, 'Content-Type': 'application/json'})
+        r = requests.get('https://api.meraki.com/api/v0/organizations/%s/inventory' % p_orgid, 
+            headers={'X-Cisco-Meraki-API-Key': p_apikey, 
+            'Content-Type': 'application/json'},
+            timeout=(REQUESTS_CONNECT_TIMEOUT, REQUESTS_READ_TIMEOUT))
     except:
-        printusertext('ERROR 04: Unable to contact Meraki cloud')
-        sys.exit(2)
+        print('ERROR 04: Unable to contact Meraki cloud')
+        return None
     
     returnvalue = []
     if r.status_code != requests.codes.ok:
-        return(returnvalue) #return empty table
+        return None
     
     rjson = r.json()
     
@@ -176,12 +141,10 @@ def main(argv):
         opts, args = getopt.getopt(argv, 'hk:f:')
     except getopt.GetoptError:
         printhelp()
-        sys.exit(2)
     
     for opt, arg in opts:
         if   opt == '-h':
             printhelp()
-            sys.exit()
         elif opt == '-k':
             arg_apikey  = arg
         elif opt == '-f':
@@ -190,7 +153,6 @@ def main(argv):
     #check if all parameters are required parameters have been given
     if arg_apikey == '' or arg_file == '':
         printhelp()
-        sys.exit(2)
        
     #set flags
     flag_processall = False
@@ -214,7 +176,7 @@ def main(argv):
         try:
             f = open(arg_file, 'r')
         except:
-            printusertext('ERROR 05: Unable to open file for reading')
+            print('ERROR 05: Unable to open file for reading')
             sys.exit(2)
         #read org names info from file
         for line in f:
@@ -226,41 +188,27 @@ def main(argv):
                 if orgid != 'null':
                     orglist[len(orglist)-1].id = orgid
                 else:
-                    printusertext('ERROR 06: Unable to resolve org ID for org name "%s"' % stripped)
-                    sys.exit(2)
+                    print('ERROR 06: Unable to resolve org ID for org name "%s"' % stripped)
         f.close()
-                        
-    #get shard host/FQDN where destination org is stored
-    #this call sometimes fails. implementing a try-verify-wait-repeat loop
-    MAX_SHARD_RESOLVE_TRIES = 5
-    for record in orglist:
-        flag_unabletoresolveshard = True
-        for i in range (0, MAX_SHARD_RESOLVE_TRIES):
-            shardhost = getshardhost(arg_apikey, record.id)
-            if shardhost == 'null':
-                time.sleep(API_EXEC_DELAY*(i+1))
-            else:
-                flag_unabletoresolveshard = False
-                break
-        if flag_unabletoresolveshard:
-            printusertext('ERROR 07: Unable to read data for org "%s"' % record.name)
-            sys.exit(2)
-        else:
-            record.shardhost = shardhost
-
-        
+                                
     for orgrecord in orglist:
-        orginventory = getorginventory(arg_apikey, orgrecord.shardhost, orgrecord.id)
-        for returnitem in orginventory:
-            orgrecord.devices.append(c_devicedata())
-            orgrecord.devices[len(orgrecord.devices)-1].serial = returnitem['serial']
-            orgrecord.devices[len(orgrecord.devices)-1].model  = returnitem['model']
+        orginventory = getorginventory(arg_apikey, orgrecord.id)
+        if not orginventory is None:
+            for returnitem in orginventory:
+                orgrecord.devices.append(c_devicedata())
+                orgrecord.devices[len(orgrecord.devices)-1].serial = returnitem['serial']
+                orgrecord.devices[len(orgrecord.devices)-1].model  = returnitem['model']
+        else:
+            print('Skipping org "%s": unable to fetch inventory' % orgrecord.name)
+            orgrecord.skipMe = True
+            
         
     for item in orglist:
-        print ('')
-        print ('Devices in org "%s"' % item.name)
-        for device in item.devices:
-            print('%s %s' % (device.serial, device.model))
+        if not item.skipMe:
+            print ('')
+            print ('Devices in org "%s"' % item.name)
+            for device in item.devices:
+                print('%s %s' % (device.serial, device.model))
             
             
     
@@ -273,28 +221,29 @@ def main(argv):
     total_count_z  = 0        
             
     for item in orglist:
-        count_mr = 0
-        count_ms = 0
-        count_mx = 0
-        count_z  = 0
-        print ('Device counts for org "%s"' % item.name)
-        for device in item.devices:
-            if device.model[:2]   == 'MR':
-                count_mr += 1
-            elif device.model[:2] == 'MS':
-                count_ms += 1
-            elif device.model[:2] == 'MX':
-                count_mx += 1
-            elif device.model[:1] == 'Z' :
-                count_z  += 1
-        total_count_mr += count_mr
-        total_count_ms += count_ms
-        total_count_mx += count_mx
-        total_count_z  += count_z 
-        print('MR: %d' % count_mr)
-        print('MS: %d' % count_ms)
-        print('MX: %d' % count_mx)
-        print('Z : %d' % count_z)
+        if not item.skipMe:
+            count_mr = 0
+            count_ms = 0
+            count_mx = 0
+            count_z  = 0
+            print ('Device counts for org "%s"' % item.name)
+            for device in item.devices:
+                if device.model[:2]   == 'MR':
+                    count_mr += 1
+                elif device.model[:2] == 'MS':
+                    count_ms += 1
+                elif device.model[:2] == 'MX':
+                    count_mx += 1
+                elif device.model[:1] == 'Z' :
+                    count_z  += 1
+            total_count_mr += count_mr
+            total_count_ms += count_ms
+            total_count_mx += count_mx
+            total_count_z  += count_z 
+            print('MR: %d' % count_mr)
+            print('MS: %d' % count_ms)
+            print('MX: %d' % count_mx)
+            print('Z : %d' % count_z)
     
     #print total device counts for all orgs
     
@@ -306,7 +255,7 @@ def main(argv):
     print('Z : %d' % total_count_z)
     
                 
-    printusertext('INFO: End of script.')
+    print('INFO: End of script.')
             
 if __name__ == '__main__':
     main(sys.argv[1:])
